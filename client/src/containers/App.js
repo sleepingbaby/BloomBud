f97f37cab0d9127d70fd498ef6282da56ba5d435
Kelvin Lau <crosby2987@gmail.com>
2024-06-07 15:25:20 -0500

    Removed redundant import

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
